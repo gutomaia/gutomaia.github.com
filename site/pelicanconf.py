caf69221dca6a66e27de24788c5b2cb07258fc2f
Guto Maia <guto@guto.net>
2015-09-24 22:47:38 -0300

    added pyNES and nodeNES links

--- site/pelicanconf.py
+++ site/pelicanconf.py
@@ -20,10 +20,10 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (
+    ('pyNES', '/pyNES'),
+    ('nodeNES', '/nodeNES'),
+)
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/gutomaia'),
